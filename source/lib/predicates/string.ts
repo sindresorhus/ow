1f7bd05795269e5bcc3f16ce05756b92f33ade15
Sam Verschueren <sam.verschueren@gmail.com>
2017-10-27 08:02:54 +0200

    Add more string validators

--- source/lib/predicates/string.ts
+++ source/lib/predicates/string.ts
@@ -1,3 +1,5 @@
+import * as valiDate from 'vali-date';
+import * as isEmailLike from 'is-email-like';
 import { Predicate, Context } from './predicate';
 
 export class StringPredicate extends Predicate<string> {
@@ -7,14 +9,98 @@ export class StringPredicate extends Predicate<string> {
 	}
 
 	/**
+	 * Test a string to have a specific minimum length.
+	 *
+	 * @param length The length of the string.
+	 */
+	length(length: number) {
+		return this.addValidator({
+			message: value => `Expected string length to be ${length}, got ${value.length}`,
+			validator: value => value.length === length
+		});
+	}
+
+	/**
 	 * Test a string to have a minimum length.
 	 *
 	 * @param length The minimum length of the string.
 	 */
 	minLength(length: number) {
 		return this.addValidator({
-			message: () => `Expected string length to be minimum ${length}`,
+			message: value => `Expected string to have a minimum length of ${length}, got ${value.length}`,
 			validator: value => value.length >= length
+		});
+	}
+
+	/**
+	 * Test a string to have a maximum length.
+	 *
+	 * @param length The maximum length of the string.
+	 */
+	maxLength(length: number) {
+		return this.addValidator({
+			message: value => `Expected string to have a maximum length of ${length}, got ${value.length}`,
+			validator: value => value.length <= length
+		});
+	}
+
+	match() {
+		// TODO
+	}
+
+	/**
+	 * Test a string to start with a specific value.
+	 *
+	 * @param searchString The value that should be the start of the string.
+	 */
+	startsWith(searchString: string) {
+		return this.addValidator({
+			message: value => `Expected ${value} to start with ${searchString}`,
+			validator: value => value.startsWith(value)
+		});
+	}
+
+	/**
+	 * Test a string to end with a specific value.
+	 *
+	 * @param searchString The value that should be the end of the string.
+	 */
+	endsWith(searchString: string) {
+		return this.addValidator({
+			message: value => `Expected ${value} to end with ${searchString}`,
+			validator: value => value.endsWith(value)
+		});
+	}
+
+	/**
+	 * Test a string to include a specific value.
+	 *
+	 * @param searchString The value that should be included in the string.
+	 */
+	includes(searchString: string) {
+		return this.addValidator({
+			message: value => `Expected ${value} to include ${searchString}`,
+			validator: value => value.includes(searchString)
+		});
+	}
+
+	empty() {
+		// TODO
+	}
+
+	nonEmpty() {
+		// TODO
+	}
+
+	/**
+	 * Test a string to be equal to a specified string.
+	 *
+	 * @param expected Expected value to match.
+	 */
+	equals(expected: string) {
+		return this.addValidator({
+			message: value => `Expected ${value} to equal to ${expected}`,
+			validator: value => value === expected
 		});
 	}
 
@@ -25,6 +111,36 @@ export class StringPredicate extends Predicate<string> {
 		return this.addValidator({
 			message: value => `Expected string to contain only alphanumeric characters but received \`${value}\``,
 			validator: value => /^[a-z\d]+$/i.test(value)
+		});
+	}
+
+	/**
+	 * Test a string to be alphanumeric.
+	 */
+	get numeric() {
+		return this.addValidator({
+			message: value => `Expected string to contain only numeric characters but received \`${value}\``,
+			validator: value => /^[\d]+$/i.test(value)
+		});
+	}
+
+	/**
+	 * Test a string to be a valid date.
+	 */
+	get date() {
+		return this.addValidator({
+			message: value => `Expected \`${value}\` to be a valid date`,
+			validator: value => valiDate(value)
+		});
+	}
+
+	/**
+	 * Test a string to be email like.
+	 */
+	get email() {
+		return this.addValidator({
+			message: value => `Expected \`${value}\` to be email like`,
+			validator: value => isEmailLike(value)
 		});
 	}
 }
