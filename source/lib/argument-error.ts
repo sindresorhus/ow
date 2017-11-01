c66131a4e91aa4b55f626ec50625bf773576ff5d
Sindre Sorhus <sindresorhus@gmail.com>
2017-11-02 01:52:36 +0700

    Tweaks to tsconfig.json (#21)
    I removed options that were the same as the default and added some missing useful ones.

--- source/lib/argument-error.ts
+++ source/lib/argument-error.ts
@@ -2,7 +2,7 @@ export class ArgumentError extends Error {
 	constructor(message, context) {
 		super(message);
 		// TODO: Node does not preserve the error name in output when using the below, why?
-		// Error.captureStackTrace(this, context);
+		Error.captureStackTrace(this, context);
 		this.name = 'ArgumentError';
 	}
 }
