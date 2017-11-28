84b4cea2a046317b29de75831bb3ccbcec82d35a
Sam Verschueren <sam.verschueren@gmail.com>
2017-11-28 18:08:40 +0100

    Process feedback

--- source/lib/predicates/error.ts
+++ source/lib/predicates/error.ts
@@ -6,6 +6,18 @@ export class ErrorPredicate extends Predicate<Error> {
 	}
 
 	/**
+	 * Test an Error to be of a specific instance type.
+	 *
+	 * @param instance The expected instance type of the error.
+	 */
+	private addErrorTypeValidator(instance: any) {
+		return this.addValidator({
+			message: error => `Expected \`${error.name}\` to be a \`${instance.name}\``,
+			validator: error => error instanceof instance
+		});
+	}
+
+	/**
 	 * Test an error to have a specific name.
 	 *
 	 * @param expected Expected name of the Error.
@@ -42,56 +54,44 @@ export class ErrorPredicate extends Predicate<Error> {
 	}
 
 	/**
-	 * Test an Error to an EvalError.
-	 */
-	get evalError() {
-		return this.addErrorTypeValidator(EvalError);
-	}
-
-	/**
-	 * Test an Error to a RangeError.
-	 */
-	get rangeError() {
-		return this.addErrorTypeValidator(RangeError);
-	}
-
-	/**
-	 * Test an Error to a ReferenceError.
-	 */
-	get referenceError() {
-		return this.addErrorTypeValidator(ReferenceError);
-	}
-
-	/**
-	 * Test an Error to a SyntaxError.
-	 */
-	get syntaxError() {
-		return this.addErrorTypeValidator(SyntaxError);
-	}
-
-	/**
-	 * Test an Error to a TypeError.
+	 * Test an Error to be a TypeError.
 	 */
 	get typeError() {
 		return this.addErrorTypeValidator(TypeError);
 	}
 
 	/**
-	 * Test an Error to a URIError.
+	 * Test an Error to be an EvalError.
 	 */
-	get uriError() {
-		return this.addErrorTypeValidator(URIError);
+	get evalError() {
+		return this.addErrorTypeValidator(EvalError);
 	}
 
 	/**
-	 * Test an Error to be of a specific instance type.
-	 *
-	 * @param instance The expected instance type of the error.
+	 * Test an Error to be a RangeError.
 	 */
-	private addErrorTypeValidator(instance: any) {
-		return this.addValidator({
-			message: error => `Expected \`${error.name}\` to be a \`${instance.name}\``,
-			validator: error => error instanceof instance
-		});
+	get rangeError() {
+		return this.addErrorTypeValidator(RangeError);
+	}
+
+	/**
+	 * Test an Error to be a ReferenceError.
+	 */
+	get referenceError() {
+		return this.addErrorTypeValidator(ReferenceError);
+	}
+
+	/**
+	 * Test an Error to be a SyntaxError.
+	 */
+	get syntaxError() {
+		return this.addErrorTypeValidator(SyntaxError);
+	}
+
+	/**
+	 * Test an Error to be a URIError.
+	 */
+	get uriError() {
+		return this.addErrorTypeValidator(URIError);
 	}
 }
