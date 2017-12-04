45aa3cfe975771d65d2adc354b2eb8ff71722be9
Sam Verschueren <sam.verschueren@gmail.com>
2017-12-04 20:19:53 +0100

    Rebase on master

--- source/index.ts
+++ source/index.ts
@@ -7,6 +7,9 @@ import {ArrayPredicate} from './lib/predicates/array';
 import {DatePredicate} from './lib/predicates/date';
 import {ErrorPredicate} from './lib/predicates/error';
 
+/**
+ * @hidden
+ */
 export type TypedArray = Int8Array | Uint8Array | Uint8ClampedArray | Int16Array | Uint16Array | Int32Array | Uint32Array | Float32Array | Float64Array;
 
 export interface Ow {
