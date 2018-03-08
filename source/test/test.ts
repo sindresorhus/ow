1cb2de95445c7486c68039e2b134ce7484c5c226
Sam Verschueren <sam.verschueren@gmail.com>
2018-03-08 17:36:17 +0100

    Add `is` predicate to provide custom validation functions

--- source/test/test.ts
+++ source/test/test.ts
@@ -8,6 +8,16 @@ test('not', t => {
 	t.throws(() => m('' as any, m.string.not.empty), '[NOT] Expected string to be empty, got ``');
 });
 
+test('is', t => {
+	const greaterThan = (max: number, x: number) => {
+		return x > max || `Expected \`${x}\` to be greater than \`${max}\``;
+	};
+
+	t.notThrows(() => m(1, m.number.is(x => x < 10)));
+	t.throws(() => m(1, m.number.is(x => x > 10)), 'Expected `1` to pass custom validation function');
+	t.throws(() => m(5, m.number.is(x => greaterThan(10, x))), 'Expected `5` to be greater than `10`');
+});
+
 test('reusable validator', t => {
 	const checkUsername = m.create(m.string.minLength(3));
 
