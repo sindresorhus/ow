c8e97145b0bccabd3775a8aa81f11214cb4d587b
Luke Travis <luke.travis@k3integrations.com>
2018-05-06 01:37:49 -0700

    Invert only the following predicate with the 'not' operator

--- source/test/test.ts
+++ source/test/test.ts
@@ -2,9 +2,11 @@ import test from 'ava';
 import m from '..';
 
 test('not', t => {
-	t.notThrows(() => m(1, m.number.not.infinite));
-	t.notThrows(() => m(1, m.number.not.infinite.greaterThan(5)));
 	t.notThrows(() => m('foo!', m.string.not.alphanumeric));
+	t.notThrows(() => m(1, m.number.not.infinite));
+	t.notThrows(() => m(1, m.number.not.infinite.not.greaterThan(5)));
+	t.throws(() => m(6, m.number.not.infinite.not.greaterThan(5)));
+	t.throws(() => m(1, m.number.not.infinite.greaterThan(5)), 'Expected 1 to be greater than 5');
 	t.throws(() => m('', m.string.not.empty), '[NOT] Expected string to be empty, got ``');
 });
 
