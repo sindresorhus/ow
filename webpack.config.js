734198af4afab9585a937e4e6e5db54dc32b9d7a
Sindre Sorhus <sindresorhus@gmail.com>
2018-09-28 13:51:23 +0700

    Update dependencies

--- webpack.config.js
+++ webpack.config.js
@@ -24,7 +24,7 @@ module.exports = {
 	plugins: [
 		new AddModuleExportsPlugin(),
 		new license.LicenseWebpackPlugin({
-			pattern: /.*/,
+			excludedPackageTest: packageName => ['webpack'].includes(packageName),
 			outputFilename: 'licenses.txt'
 		})
 	],
