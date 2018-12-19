8ae048c4931dfd51b496cefb40b24c78d3722be6
Sindre Sorhus <sindresorhus@gmail.com>
2018-12-19 15:43:13 +0100

    Bump dependencies

--- source/lib/predicates/base-predicate.ts
+++ source/lib/predicates/base-predicate.ts
@@ -9,5 +9,6 @@ export const testSymbol = Symbol('test');
  * @hidden
  */
 export interface BasePredicate<T = any> {
+	// tslint:disable-next-line completed-docs
 	[testSymbol](value: T, main: Ow): void;
 }
