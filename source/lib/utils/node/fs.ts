c85f805efbdbfb77ef41c4246eb6bea25b7cebb7
Sam Verschueren <sam.verschueren@gmail.com>
2019-01-08 07:57:01 +0100

    Add docs

--- source/lib/utils/node/fs.ts
+++ source/lib/utils/node/fs.ts
@@ -1,3 +1,4 @@
 import nodeRequire from './require';
 
+// Re-export Node.js fs module to make sure it doesn't get bundled with front-end applications
 export default nodeRequire('fs');
--- source/lib/utils/node/require.ts
+++ source/lib/utils/node/require.ts
@@ -1,3 +1,4 @@
+// Export `__non_webpack_require__` in Webpack environments to make sure it doesn't bundle modules loaded via this method
 export default typeof (global as any).__non_webpack_require__ === 'function'
 	? (global as any).__non_webpack_require__
 	: require;
