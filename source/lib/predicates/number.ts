b43e954a6fdb3cc622a36610f183cf9470b0d680
Luke Childs <lukechilds123@gmail.com>
2019-01-12 15:19:30 +0700

    Use descriptive type names in comments

--- source/lib/predicates/number.ts
+++ source/lib/predicates/number.ts
@@ -143,42 +143,42 @@ export class NumberPredicate extends Predicate<number> {
 	}
 
 	/**
-	 * Test a number to be in a valid range for a uint8.
+	 * Test a number to be in a valid range for a 8-bit unsigned integer.
 	 */
 	get uint8() {
 		return this.integer.inRange(0, 255);
 	}
 
 	/**
-	 * Test a number to be in a valid range for a uint16.
+	 * Test a number to be in a valid range for a 16-bit unsigned integer.
 	 */
 	get uint16() {
 		return this.integer.inRange(0, 65535);
 	}
 
 	/**
-	 * Test a number to be in a valid range for a uint32.
+	 * Test a number to be in a valid range for a 32-bit unsigned integer.
 	 */
 	get uint32() {
 		return this.integer.inRange(0, 4294967295);
 	}
 
 	/**
-	 * Test a number to be in a valid range for a int8.
+	 * Test a number to be in a valid range for a 8.-bit signed integer.
 	 */
 	get int8() {
 		return this.integer.inRange(-128, 127);
 	}
 
 	/**
-	 * Test a number to be in a valid range for a int16.
+	 * Test a number to be in a valid range for a 16-bit signed integer.
 	 */
 	get int16() {
 		return this.integer.inRange(-32768, 32767);
 	}
 
 	/**
-	 * Test a number to be in a valid range for a int32.
+	 * Test a number to be in a valid range for a 32-bit signed integer.
 	 */
 	get int32() {
 		return this.integer.inRange(-2147483648, 2147483647);
