e930bdc7ebe5c93f146aea33df34d85ff411133b
Sam Verschueren <sam.verschueren@gmail.com>
2019-02-01 09:16:27 +0100

    Improve custom require logic

--- source/lib/utils/node/require.ts
+++ source/lib/utils/node/require.ts
@@ -1,4 +1,13 @@
-// Export `__non_webpack_require__` in Webpack environments to make sure it doesn't bundle modules loaded via this method
-export default typeof (global as any).__non_webpack_require__ === 'function'
-	? (global as any).__non_webpack_require__
-	: eval('require'); // tslint:disable-line:no-eval
+let customRequire: (packageName: string) => any;
+
+try {
+	// Export `__non_webpack_require__` in Webpack environments to make sure it doesn't bundle modules loaded via this method
+	customRequire = (global as any).__non_webpack_require__ === 'function'
+		? (global as any).__non_webpack_require__
+		: eval('require'); // tslint:disable-line:no-eval
+} catch {
+	// Use a noop in case both `__non_webpack_require__` and `require` does not exist
+	customRequire = () => {}; // tslint:disable-line:no-empty
+}
+
+export default customRequire;
