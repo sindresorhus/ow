e697ccdd4f9e7bba71d15ee4de1ed40c00d67acb
Sam Verschueren <sam.verschueren@gmail.com>
2019-04-01 10:12:35 +0200

    Only call captureStackTrace when possible (#141)

--- source/argument-error.ts
+++ source/argument-error.ts
@@ -5,7 +5,9 @@ export class ArgumentError extends Error {
 	constructor(message: string, context: Function) {
 		super(message);
 		// TODO: Node does not preserve the error name in output when using the below, why?
-		Error.captureStackTrace(this, context);
+		if ('captureStackTrace' in Error) {
+			Error.captureStackTrace(this, context);
+		}
 		this.name = 'ArgumentError';
 	}
 }
