a58814cb9c3ec18c584b10a73fdd60bdd8f4e5ee
Alexander Buiko <reed665@hotmail.com>
2019-11-12 09:54:51 +0300

    Preserve argument error stack trace in the browser (#164)

--- source/argument-error.ts
+++ source/argument-error.ts
@@ -5,8 +5,12 @@ export class ArgumentError extends Error {
 	constructor(message: string, context: Function) {
 		super(message);
 
-		// TODO: Node.js does not preserve the error name in output when using the below, why?
-		Error.captureStackTrace?.(this, context);
+		if (Error.captureStackTrace) {
+			// TODO: Node.js does not preserve the error name in output when using the below, why?
+			Error.captureStackTrace(this, context);
+		} else {
+			this.stack = (new Error()).stack;
+		}
 
 		this.name = 'ArgumentError';
 	}
