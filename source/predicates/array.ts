b2ebe6f6f631b104b4c8458f3a34b01dc83524cb
Sindre Sorhus <sindresorhus@gmail.com>
2020-12-26 17:17:58 +0700

    Update array.ts

--- source/predicates/array.ts
+++ source/predicates/array.ts
@@ -162,9 +162,9 @@ export class ArrayPredicate<T = unknown> extends Predicate<T[]> {
 	}
 
 	/**
-	Test all elements in the array to match the exact shape with the provided predicate.
+	Test all elements in the array to match the exact shape of the provided predicate.
 
-	The predicates is an array which describes how the tested array should look like. Each predicate placed at the nth index should be applied also to the item at the same index.
+	The predicates is an array which describes how the tested array should look like. Each predicate placed at the nth index should be applied to the item at the same index.
 
 	@param predicates - The predicate placed at the nth index should be applied also to the item at the same index.
 
