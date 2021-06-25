baab8916da0d140a15eb3093c48a51bd60dbaf34
Hans Hsieh <539874+hanshsieh@users.noreply.github.com>
2021-06-25 18:05:32 +0800

    Merge the logic of `of-type-with-name` into `of-type`

--- source/utils/of-type.ts
+++ source/utils/of-type.ts
@@ -1,4 +1,5 @@
 import ow from '..';
+import test from '../test';
 import {BasePredicate} from '../predicates/base-predicate';
 
 /**
@@ -7,11 +8,17 @@ Test all the values in the collection against a provided predicate.
 @hidden
 @param source Source collection to test.
 @param predicate Predicate to test every item in the source collection against.
+@param name The name to call the collection of values, such as "values", "keys".
+            If it is 'undefined', it uses the call stack to infer the label.
 */
-export default <T>(source: IterableIterator<T> | Set<T> | T[], predicate: BasePredicate<T>): boolean | string => {
+export default <T>(source: IterableIterator<T> | Set<T> | T[], predicate: BasePredicate<T>, name?: string): boolean | string => {
 	try {
 		for (const item of source) {
-			ow(item, predicate);
+			if (name) {
+				test(item, name, predicate, false);
+			} else {
+				ow(item, predicate);
+			}
 		}
 
 		return true;
