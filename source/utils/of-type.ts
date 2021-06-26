428fa18011fd82338cc9726a6c3e1baac9fa945b
Sindre Sorhus <sindresorhus@gmail.com>
2021-06-26 17:40:18 +0700

    Update of-type.ts

--- source/utils/of-type.ts
+++ source/utils/of-type.ts
@@ -2,9 +2,9 @@ import ow from '..';
 import test from '../test';
 import {BasePredicate} from '../predicates/base-predicate';
 
+// TODO: After we migrate all usages of this function to specify the optional 'name' parameter, we can change the parameter to be required.
 /**
 Test all the values in the collection against a provided predicate.
-TODO After we migrate all usages of this function to specify the optional 'name' parameter, we can change the parameter to be required.
 
 @hidden
 @param source Source collection to test.
